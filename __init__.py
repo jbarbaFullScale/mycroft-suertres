51140fd08473ea6f7cfb3979cf11b1e259f3644c
Johhan Barba <jbarba@fullscale.io>
2019-06-19 13:00:58 +0800

    initial commit

--- __init__.py
+++ __init__.py
@@ -20,42 +20,48 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 
-__author__ = 'eward'
+__author__ = 'Johhan'
 
 LOGGER = getLogger(__name__)
 
 
-class HelloWorldSkill(MycroftSkill):
+class FunSkill(MycroftSkill):
     def __init__(self):
-        super(HelloWorldSkill, self).__init__(name="HelloWorldSkill")
+        super(FunSkill, self).__init__(name="FunSkill")
 
     def initialize(self):
-        thank_you_intent = IntentBuilder("ThankYouIntent"). \
-            require("ThankYouKeyword").build()
-        self.register_intent(thank_you_intent, self.handle_thank_you_intent)
+        self.load_data_files(dirname(__file__))
 
-        how_are_you_intent = IntentBuilder("HowAreYouIntent"). \
-            require("HowAreYouKeyword").build()
-        self.register_intent(how_are_you_intent,
-                             self.handle_how_are_you_intent)
+        fun_intent = IntentBuilder("FunIntent").
+            require("FunKeyword").build()
+        self.register_intent(fun_intent, self.handle_fun_intent)
+        # thank_you_intent = IntentBuilder("ThankYouIntent"). \
+        #     require("ThankYouKeyword").build()
+        # self.register_intent(thank_you_intent, self.handle_thank_you_intent)
 
-        hello_world_intent = IntentBuilder("HelloWorldIntent"). \
-            require("HelloWorldKeyword").build()
-        self.register_intent(hello_world_intent,
-                             self.handle_hello_world_intent)
+        # how_are_you_intent = IntentBuilder("HowAreYouIntent"). \
+        #     require("HowAreYouKeyword").build()
+        # self.register_intent(how_are_you_intent,
+        #                      self.handle_how_are_you_intent)
 
-    def handle_thank_you_intent(self, message):
-        self.speak_dialog("welcome")
+        # hello_world_intent = IntentBuilder("HelloWorldIntent"). \
+        #     require("HelloWorldKeyword").build()
+        # self.register_intent(hello_world_intent,
+        #                      self.handle_hello_world_intent)
 
-    def handle_how_are_you_intent(self, message):
-        self.speak_dialog("how.are.you")
 
-    def handle_hello_world_intent(self, message):
-        self.speak_dialog("hello.world")
+    def handle_fun_intent(self, message):
+        self.speak_dialog("Mia Khalifa, She can sing high notes with no efforts")
+
+    # def handle_how_are_you_intent(self, message):
+    #     self.speak_dialog("how.are.you")
+
+    # def handle_hello_world_intent(self, message):
+    #     self.speak_dialog("hello.world")
 
     def stop(self):
         pass
 
 
 def create_skill():
-    return HelloWorldSkill()
+    return FunSkill()
